2ecc676f802362b7def13559858d9b7f0f36a4e8
RicardoGMiguel <ricardo.miguel@sp.senai.br>
2023-08-18 16:52:28 -0300

    refactor: remove comments

--- opcua-server.ts
+++ opcua-server.ts
@@ -1,12 +1,6 @@
 import { OPCUAServer, DataType } from 'node-opcua'
 import 'dotenv/config';
 
-
-// const server = new OPCUAServer({
-//     port: Number(process.env.OPCUA_PORT) || 62640,
-//     resourcePath: process.env.RESOURCE_PATH || "/UA/MyServer",
-// });
-
 const server = new OPCUAServer({
     hostname: process.env.OPCUA_HOSTNAME || '192.168.1.8',
     port: Number(process.env.OPCUA_PORT) || 62640,
